dcaca55049e2f08cf0b03a46a4fa7766ffbc7ded
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2025-04-14 16:50:50 +0500

    added login and signup

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/Landing Page/LandingPage";
 import AboutUs from "./components/About/AboutUs";
 import ContactUs from "./components/contactUS/ContactUs";
+import Login from "./components/signin/Login";
 const AppRouter = () => {
   return (
     <>
@@ -11,6 +12,8 @@ const AppRouter = () => {
           <Route path='/' element={<LandingPage />} />
           <Route path='/about' element={<AboutUs />} />
           <Route path='/contact' element={<ContactUs />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
         </Routes>
       </Router>
     </>
